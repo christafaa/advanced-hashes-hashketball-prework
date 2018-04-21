4573c2a787f21f97f92b0148b0707067e380b876
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 17:44:59 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -26,15 +26,15 @@ def game_hash
           blocks: 12,
           slam_dunks: 7
         },
-        "" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Brook Lopez" => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
         },
         "" => {
           number: ,
