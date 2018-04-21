1f54c61fbd98c9bd00fe275f7016caa06f64bffc
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 17:45:33 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,15 +36,15 @@ def game_hash
           blocks: 1,
           slam_dunks: 15
         },
-        "" => {
-          number: ,
-          shoe: ,
-          points: ,
-          rebounds: ,
-          assists: ,
-          steals: ,
-          blocks: ,
-          slam_dunks:
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
         },
         "" => {
           number: ,
