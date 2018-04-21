ffb16b28a655137b1cf12362800a7686ff05ff03
Chris Alberti <christopherjalberti@gmail.com>
2018-04-21 18:44:58 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,7 +228,6 @@ def player_with_longest_name
   game_hash.each do |location, team_data|
     location[:players].each do |player_name, player_data|
       if name == nil || player_name.length > name.length
-        name_length = points
         name = player_name
       end
     end
